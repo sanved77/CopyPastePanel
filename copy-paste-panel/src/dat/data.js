b1bab0d3a523bd38627e8a000acb89cff03d7ae3
sanved77 <sanved77@gmail.com>
2020-07-13 21:47:48 -0400

    Added new web resume descriptions

--- copy-paste-panel/src/dat/data.js
+++ copy-paste-panel/src/dat/data.js
@@ -34,18 +34,18 @@ const panels = {
             {
                 label: 'Binghamton',
                 picture: 'fas fa-school',
-                text: '- Reduced manual computation by 90% by developing Android and iOS apps for telemedicine using Java and Swift in Android Studio and Xcode, that led to 15%-20% increase in efficiency\n' +
-                '- Developed web applications using Bootstrap, Bulma and PHP, MySQL as the backend to support the Android/iOS apps',
+                text: '- Developed web applications as telemedicine tools using Bootstrap, Bulma and PHP, MySQL as the backend, that reduced manual computation by 90% and led to 15%-20% increase in efficiency\n' +
+                '- Developed Android/iOS clients using Java and Swift in Android Studio and Xcode that utilized the web apps',
                 color: '#006600',
                 bigText: true
             },
             {
                 label: 'Core',
                 picture: 'fas fa-circle-notch',
-                text: '- Developed a Business Management Tool using ActionHero, Vue, Nginx, PostgreSQL, Node to eliminate manual process\n' + 
-                '- Developed an Android/iOS app for 10,000+ daily users with NativeScript-Vue for surveying and automatic reporting\n' +
+                text: '- Developed a business management web app with 25,000+ users using ActionHero, Vue, Nginx, PostgreSQL, Node\n' + 
                 '- Achieved 40% quicker load time compared to the legacy system with pre/post processing middleware in REST calls\n' +
                 '- Provided 100% test coverage by writing test modules for REST APIs using Mocha and Chai test suite\n' +
+                '- Developed an Android/iOS app for 10,000+ daily users with NativeScript-Vue for surveying and automatic reporting\n' +
                 '- Contributed in Design Meetings and discussions to analyze and translate client requirements and familiarized with daily stand-ups, retrospection meetings, and 10-day sprints following Agile Software Development Process',
                 color: '#47b012',
                 bigText: true
@@ -53,15 +53,16 @@ const panels = {
             {
                 label: 'EnhanceVR',
                 picture: 'fas fa-vr-cardboard',
-                text: '- Designed, developed and maintained Enhance VR’s web portal using MongoDB, Express, React, Node (MERN) stack and deployed it using Heroku and reduced load by 50% by using load balancing techniques',
+                text: '- Designed, developed, and maintained Enhance VR’s web portal using MongoDB, Express, React, Node (MERN) stack and performed state management using Redux with a focus on UX and UI design\n' +
+                '- Created a portal using EJS on Node for managing multiple Virtual Reality headsets through REST API assistance',
                 color: '#ff7400',
                 bigText: true
             },
             {
                 label: 'Wiseass',
                 picture: 'fas fa-democrat',
-                text: '- Developed various Android apps for business management assisted by cloud application services (SaaS) which were designed using Node, Express and MongoDB and optimized with load balancers and microservices architecture\n' +
-                '- Built business development strategies and oversaw the end to end process for client acquisition and sales for 15+ clients',
+                text: '- Developed various Cloud Application Services (SaaS) which were designed using Node, Express and MongoDB and optimized with load balancers and microservices architecture assisting Android apps (Java) for Business Management\n' +
+                '- Served 15+ clients by developing business strategies, and oversaw end to end process for client acquisition and sales',
                 color: '#00abff',
                 bigText: true
             }
@@ -84,7 +85,7 @@ const panels = {
                 picture: 'fas fa-globe',
                 text: 'Hello AAAA !\n' +
                 'I am sure you must be incredibly busy and must be getting a lot of emails, but this will only take 60 seconds to read.\n\n' + 
-                'I am a graduate student and have completed my Masters in Computer Science this May and I have 2+ years of experience as a Software Engineer focusing on development of web apps and large scale systems supporting 15,000+ daily users. My expertise lies in JavaScript ES6, Java and web frameworks like Node, React and Vue.\n\n' +
+                'I am a graduate student and have completed my Masters in Computer Science this May and I have 2+ years of experience as a Software Engineer focusing on development of web apps and large scale systems supporting 25,000+ daily users. My expertise lies in JavaScript ES6, Java and web frameworks like Node, React and Vue.\n\n' +
                 'The XXXX position at YYYY, ZZZZ aligns with my skill set and I believe I would be a productive asset to your company. Would you be willing to share my LinkedIn profile and resume with the hiring team and let them know that I\'ve applied? I would obliged if you could put in a good word.\n\n' +
                 'I can totally understand if you are busy and can\'t respond, but even a 1-2 line reply would mean a lot to me. I am attaching my resume for your reference. Happy to chat more if you have the time.\n\n' +                
                 'Thank you.',
@@ -96,7 +97,7 @@ const panels = {
                 picture: 'fas fa-globe',
                 text: 'Hello AAAA !\n' +
                 'I am sure you must be incredibly busy and must be getting a lot of emails, but this will only take 60 seconds to read.\n\n' + 
-                'I am a graduate student and have completed my Masters in Computer Science this May and I have 2+ years of experience as a Software Engineer focusing on development of web apps and large scale systems supporting 15,000+ daily users. My expertise lies in JavaScript ES6, Java and web frameworks like Node, React and Vue.\n\n' +
+                'I am a graduate student and have completed my Masters in Computer Science this May and I have 2+ years of experience as a Software Engineer focusing on development of web apps and large scale systems supporting 25,000+ daily users. My expertise lies in JavaScript ES6, Java and web frameworks like Node, React and Vue.\n\n' +
                 'I was wondering if there are any open positions in your team which align with my skill set. I believe I would be a productive asset to your company. Please let me know if you’d be open to a conversation to discuss potential opportunities.\n\n' +
                 'I can totally understand if you are busy and can\'t respond, but even a 1-2 line reply would mean a lot to me. I am attaching my resume for your reference. Happy to chat more if you have the time.\n\n' +                
                 'Thank you.',
